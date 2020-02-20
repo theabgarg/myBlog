1d4a4c073ebb1328c4582eb028a72dbd03dc509c
marshalcode <abhishekgarg502@gmail.com>
2020-02-20 18:37:09 +0530

    'date'

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,29 @@
 <?php
     session_start();
     print_r($_SESSION);
+
+    include("../api/user.api.php");
+
+    $newUser = new user;
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="../assets/js/jquery.js"></script>
+</head>
+<body>
+    <button onclick="generate()" >generate signup token</button>
+    <div class="response"></div>
+    <script>
+        function generate(){
+            var token = <?php echo $newUser->authToken();?>;
+            var htmldata = "https://cseprofessor.ml/signup?token=" + token;
+            $(".response").append(htmldata);
+        }
+    </script>
+</body>
+</html>

1863ddce50339b93b0b41aeb29169328712822cb
marshalcode <abhishekgarg502@gmail.com>
2020-02-20 17:13:02 +0530

    'date'

--- conn/conn.php
+++ conn/conn.php
@@ -3,18 +3,12 @@
 $servername = "localhost";
 $username = "uysrd6vjvtjmb";
 $password = "veroxyle@123456";
+$database = "dbs7quzkph56gw";
 
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $database);
 
 if($conn->connect_error){
     die("connection failed: " . $conn->connect_error);
-}
-
-try {
-    $conn->select_db('myBlog');
-}
-catch (mysqli_sql_exception $e){
-    echo "caught exception: " . $e->getMessage() . "\n";
 }
 
 ?>

c04f021bcfa5aef37d27814da6743e030b683274
marshalcode <abhishekgarg502@gmail.com>
2020-02-21 11:55:49 +0530

    'date'

--- index.php
+++ index.php
@@ -13,19 +13,17 @@
     <link rel="stylesheet" href="assets/css/main.css">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    
     <style>
         ul{}
-        ul a{ }
-        ul a li{ }
+        ul a{}
+        ul a li{}
         .active{background-color:#d90000;}
     </style>
-
     <script type="text/javascript">
         $(document).ready(function(){
             $('ul a li').click(function(){
-                $('a li').removeClass("active");
-                $(this).addClass("active");
+                $('a li').css("background-color", "none");
+                $(this).css("background-color", "#d90000");
             });
         });
     </script>

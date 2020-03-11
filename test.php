d6950d8bb79590e3e695fe444d2f74265963a982
marshalcode <abhishekgarg502@gmail.com>
2020-03-11 21:45:07 +0530

    'date'

--- test.php
+++ test.php
@@ -1,1 +1,12 @@
-<form method="post"><label for="title">Title</label><input type="text" name="title" id="title"><label for="image">Image</label><input type="file" name="image" id="image"><label for="desc">Short Description</label><input type="text" name="description" id="description"><label for="category">Select Category</label><select name="category" id="category"><option value="how_to">How To</option><option value="news">News</option><option value="reviews">Reviews</option><option value="blog">Blog</option></select><label for="publish">Publish Now?</label><input type="checkbox" name="publish" id="publish"><input type="submit" value="submit"></form>
+<form method="post">
+    <label for="title">Title</label>  <br>
+    <input type="text" name="title" id="title"> <br> 
+    <label for="image">Image</label><br>
+    <input type="file" name="image" id="image"><br>
+    <label for="desc">Short Description</label><br>
+    <input type="text" name="description" id="description"><br>
+    <label for="category">Select Category</label><br>
+    <select name="category" id="category"><option value="how_to">How To</option><option value="news">News</option><option value="reviews">Reviews</option><option value="blog">Blog</option></select><br>
+    <label for="publish">Publish Now?</label><br>
+    <input type="checkbox" name="publish" id="publish"><br>
+    <input type="submit" value="submit"></form>

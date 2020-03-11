9706a613e0f667d359cb8d464c9cd3b1824917f4
marshalcode <abhishekgarg502@gmail.com>
2020-03-11 15:14:30 +0530

    'date'

--- test.php
+++ test.php
@@ -4,10 +4,13 @@
     padding:0;
 }
 #footer{
-    bottom:0;
-    left:0;
-    height:20%;
-    background-color:black;
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: black;
+    color: white;
+    text-align: center;
 }
 </style>
 

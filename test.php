cd85f011682b7520f0e196a99c4a27ecc0247fc9
marshalcode <abhishekgarg502@gmail.com>
2020-03-11 15:42:35 +0530

    'date'

--- test.php
+++ test.php
@@ -1,92 +1,28 @@
 <style>
-*{
-    margin:0;
-    padding:0;
-}
-#footer{
-    position: fixed;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    background-color: black;
-    color: white;
-    text-align: center;
-    padding:auto;
-}
+    *{
+        margin:0;
+        padding:0;
+    }
 
-ul{
-    list-style-type:none;
-}
-
-.footer{
-    width:100%;
-}
-
-.footer .flist{
-    width:auto;
-    float:left;
-    margin:auto;
-    padding:auto;
-}
+    footer{
+        bottom:0;
+        left:0;
+        padding: 10px 0px;
+        width:100%;
+    }
 </style>
 
-
-<div id="footer">
-<ul class="footer">
-    <li class="flist">
-        <ul>
-            <span>menu1</span>
-            <li>lorem1</li>
-            <li>lorem2</li>
-            <li>lorem3</li>
-            <li>lorem4</li>
-            <li>lorem5</li>
-            <li>lorem6</li>
-        </ul>
-    </li>
-    <li class="flist">
-        <ul>
-            <span>menu2</span>
-            <li>lorem1</li>
-            <li>lorem2</li>
-            <li>lorem3</li>
-            <li>lorem4</li>
-            <li>lorem5</li>
-            <li>lorem6</li>
-        </ul>
-    </li>
-    <li class="flist">
-        <ul>
-            <span>menu3</span>
-            <li>lorem1</li>
-            <li>lorem2</li>
-            <li>lorem3</li>
-            <li>lorem4</li>
-            <li>lorem5</li>
-            <li>lorem6</li>
-        </ul>
-    </li>
-    <li class="flist">
-        <ul>
-            <span>menu4</span>
-            <li>lorem1</li>
-            <li>lorem2</li>
-            <li>lorem3</li>
-            <li>lorem4</li>
-            <li>lorem5</li>
-            <li>lorem6</li>
-        </ul>
-    </li>
-    <li class="flist">
-        <ul>
-            <span>menu5</span>
-            <li>lorem1</li>
-            <li>lorem2</li>
-            <li>lorem3</li>
-            <li>lorem4</li>
-            <li>lorem5</li>
-            <li>lorem6</li>
-        </ul>
-    </li>
-</ul>
-</div>
+<footer>
+    <div class="wrapper">
+        <div class="sitemap">
+            <ul>
+                <li>home</li>
+                <li>news</li>
+                <li>how_to</li>
+                <li>review</li>
+                <li>blog</li>
+                <li>contact</li>
+            </ul>
+        </div>
+    </div>
+</footer>

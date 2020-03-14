86b060bab1475a25b4470f928b44dbad30e053b8
marshalcode <abhishekgarg502@gmail.com>
2020-03-14 10:05:44 +0530

    'date'

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
                     var author = "<?php echo $data['username'];?>";
                     var date = "<?php echo $data['post_date'];?>";
                     // var postDate = new Date(date);
-                    var post = postDesign(img, title, shortDesc, username, date);
+                    var post = postDesign(img, title, shortDesc, author, date);
                     document.write(post);
                     </script>
                     "after script

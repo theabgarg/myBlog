ba8abca07cbaecfafaed9e6831bd3af1967ce314
marshalcode <abhishekgarg502@gmail.com>
2020-03-23 20:15:29 +0530

    'date'

--- test.php
+++ test.php
@@ -82,7 +82,11 @@
     </div>
 
     <footer class="footer">
-        <div class="footer-content"></div>
+        <div class="footer-content">
+            <div class="footer-section about"></div>
+            <div class="footer-section links"></div>
+            <div class="footer-section contact-form"></div>
+        </div>
 
         <div class="footer-bottom">
             &copy; veroxyle | Designed by abhishek garg, abhishek kishor, MD sharib, aniket ch.

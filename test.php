7d0a522bff39703f0d63e8c88671c2531e3b3466
marshalcode <abhishekgarg502@gmail.com>
2020-03-23 17:22:29 +0530

    'date'

--- test.php
+++ test.php
@@ -8,7 +8,8 @@
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/main2.css">
     <script src="assets/js/jquery.js"></script>
-    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <!-- <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script> -->
+    <script src="assets/js/slick.js"></script>
 </head>
 <body>
     <header>

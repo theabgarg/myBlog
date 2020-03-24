27a6d72ed2ace3b4449e3f7d1e6126fe1f8071b0
marshalcode <abhishekgarg502@gmail.com>
2020-03-24 10:51:00 +0530

    'date'

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Blog</title>
+    <link rel='shortcut icon' href='favicon.png' type='image/x-icon' />
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/main2.css">
     <script src="assets/js/jquery.js"></script>

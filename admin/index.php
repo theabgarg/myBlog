b061a01b4ced2475f8a80a0d6326fcea180eb2ef
marshalcode <abhishekgarg502@gmail.com>
2020-03-24 11:26:10 +0530

    'date'

--- admin/index.php
+++ admin/index.php
@@ -23,7 +23,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="../assets/js/jquery.js"></script>
-    <script src="../assets/js/main.js"></script>
+    <!-- <script src="../assets/js/main.js"></script> -->
     <!-- <link rel="stylesheet" href="../assets/css/main.css"> -->
     <link rel="stylesheet" href="main.css">
 </head>

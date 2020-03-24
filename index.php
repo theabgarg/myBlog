040e4fbb9466c34972a44de8f266978788fa8269
marshalcode <abhishekgarg502@gmail.com>
2020-03-24 09:43:50 +0530

    'date'

--- index.php
+++ index.php
@@ -80,6 +80,18 @@
             <div class="sidebar"></div>
         </div>
     </div>
+    <div class="card">
+            <img src="assets/images/image_1.png" alt="" class="post-image">
+            <div class="post-info">
+                <div class="post-title"><h2><a href="single.php">the rain drops and the light shines.</a></h2></div>
+                <div class="post-description">the rain drop poem is amazing fabulous Lorem ipsum dolor, sit amet consectetur adipisicing elit.</div>
+                <div class="post-author">
+                    <i class="fa fa-user">veroXyle</i>
+                    &nbsp;
+                    <i class="fa fa-calendar">mar 30, 2020</i>
+                </div>
+            </div>
+        </div>
 
     <footer class="footer">
         <div class="footer-content">

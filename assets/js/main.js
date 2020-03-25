369175754168b06e610b76f8b1273e42e13c3cfc
marshalcode <abhishekgarg502@gmail.com>
2020-03-25 19:18:20 +0530

    'date'

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,7 @@ function footer(){
 }
 
 function postform(){
-    var x = '<form class="new-post-form" method="post"><label for="title">Title</label>  <br><input type="text" name="title" id="title"> <br> <label for="image">Image</label><br><input type="file" name="image" id="image"><br><label for="desc">Short Description</label><br><input type="text" name="description" id="description"><br><label for="category">Select Category</label><br><select name="category" id="category"><option value="how_to">How To</option><option value="news">News</option><option value="reviews">Reviews</option><option value="blog">Blog</option></select><br><input type="checkbox" name="publish" id="publish"><label for="publish">Publish Now?</label><br><input type="submit" value="submit"></form>';
+    var x = '<form class="new-post-form" method="post"><label for="title">Title</label>  <br><input type="text" name="title" id="title"> <br> <label for="image">Image</label><br><input type="file" name="image" id="image"><br><label for="desc">Short Description</label><br><input type="text" name="description" id="description"><br><label for="category">Select Category</label><br><select name="category" id="category"><option value="how_to">How To</option><option value="news">News</option><option value="reviews">Reviews</option><option value="blog">Blog</option></select><br><input type="checkbox" name="publish" id="publish">Publish Now?<br><input type="submit" value="submit"></form>';
     return x;
 }
 

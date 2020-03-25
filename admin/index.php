35a9274c891c7231cd8e96a234c5856cffc5d31b
marshalcode <abhishekgarg502@gmail.com>
2020-03-25 20:30:41 +0530

    'date'

--- admin/index.php
+++ admin/index.php
@@ -24,10 +24,40 @@
     <title>Document</title>
     <script src="../assets/js/jquery.js"></script>
     <script src="../assets/js/main.js"></script>
-    <!-- <link rel="stylesheet" href="../assets/css/main.css"> -->
+    <link rel="stylesheet" href="../assets/css/main2.css">
     <link rel="stylesheet" href="main.css">
 </head>
 <body>
+<header>
+        <div class="logo">
+            <img src="assets/images/logo.png" alt="logo" height="48" width="48">
+            <h1 class="logo-text">vero<span>xyle</span></h1>
+        </div>
+
+        <i class="fa fa-bars menu-toggle"></i>
+        <i class="fa fa-times menu-close"></i>
+
+        <ul class="nav">
+            <li><a href="#">Latest</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">How-to</a></li>
+            <li><a href="#">Reviews</a></li>
+            <li><a href="#">Blog</a></li>
+            <!-- <li><a href="#">Sign Up</a></li>
+            <li><a href="#">Login</a></li> -->
+            <li>
+                <a href="#">
+                <i class="fa fa-user"></i>    
+                veroxyle
+                <i class="fa fa-chevron-down" style="font-size: 0.8em;"></i>
+                </a>
+                <ul>
+                    <li><a href="#">Dashboard</a></li>
+                    <li><a href="#" class="logout">Logout</a></li>
+                </ul>
+            </li>
+        </ul>
+    </header>
 
 <script>
     var htmldata='a';

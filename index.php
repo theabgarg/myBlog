0bc04d4af91332ecb58187f304294124031a13bd
marshalcode <abhishekgarg502@gmail.com>
2020-04-02 19:45:58 +0530

    'date'

--- index.php
+++ index.php
@@ -14,36 +14,7 @@
     <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 </head>
 <body>
-    <header>
-        <div class="logo">
-            <img src="assets/images/logo.png" alt="logo" height="48" width="48">
-            <h1 class="logo-text">vero<span>xyle</span></h1>
-        </div>
-
-        <i class="fa fa-bars menu-toggle"></i>
-        <i class="fa fa-times menu-close"></i>
-
-        <ul class="nav">
-            <li><a href="#">Latest</a></li>
-            <li><a href="#">News</a></li>
-            <li><a href="#">How-to</a></li>
-            <li><a href="#">Reviews</a></li>
-            <li><a href="#">Blog</a></li>
-            <!-- <li><a href="#">Sign Up</a></li>
-            <li><a href="#">Login</a></li> -->
-            <li>
-                <a href="#">
-                <i class="fa fa-user"></i>    
-                veroxyle
-                <i class="fa fa-chevron-down" style="font-size: 0.8em;"></i>
-                </a>
-                <ul>
-                    <li><a href="#">Dashboard</a></li>
-                    <li><a href="#" class="logout">Logout</a></li>
-                </ul>
-            </li>
-        </ul>
-    </header>
+    <?php include("includes/header.php"); ?>
 
     <div class="page-wrapper">
         <?php include("includes/posts.php") ?>
